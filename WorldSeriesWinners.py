d1d720c2a2f61b9cffbcd1962375a4b164e24702
Nichol Vacca <nichol_vacca1@baylor.edu>
2022-02-16 14:49:36 -0600

    hoem

--- WorldSeriesWinners.py
+++ WorldSeriesWinners.py
@@ -7,7 +7,12 @@ def main():
     counter = 0 
     
     for team in winner:
-        result  = counter + 1
+        if team == winner:
+            result  = counter + 1
 
-        if result == 1:
-            print('The', team, "won the world series", result, "times between 1903 and 2009")
+            if result == 1:
+                print('The', team, "won the world series", result, "times between 1903 and 2009")
+            elif result > 1:
+                print('The', team, "won the world series", result, "times between 1903 and 2009")
+            else:
+                print('The', team, 'never won the world series')
